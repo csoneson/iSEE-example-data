734b99c300ea65fede0bccd317f1488c9c7785b6
Charlotte Soneson <charlottesoneson@gmail.com>
2025-06-27 14:01:51 -0400

    Add second configuration for Allen

--- allen/allen_initial_2.R
+++ allen/allen_initial_2.R
@@ -1,10 +1,11 @@
 library(iSEE)
 
-initial <- list(ReducedDimensionPlot(PanelWidth=6L,
-                                     Type="PCA",
+initial <- list(ReducedDimensionPlot(PanelWidth=4L,
+                                     Type="TSNE",
                                      ColorBy="Column data",
                                      ColorByColumnData="Core.Type"),
-                FeatureAssayPlot(PanelWidth=6L,
+                ColumnDataTable(),
+                FeatureAssayPlot(PanelWidth=4L,
                                  XAxis="Column data",
                                  XAxisColumnData="Core.Type",
                                  YAxisFeatureName="Cd80"))
